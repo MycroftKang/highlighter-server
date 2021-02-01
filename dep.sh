f2608a916526d45e1932ee62e955043a280a1038
Taehyeok Kang <taet777@naver.com>
2021-02-02 01:10:18 +0900

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,1 +1,22 @@
+if [ ! -d ~/.ssh ]; then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+fi
+
+base64 --decode ${CUSTOM_SSH_KEY} > ~/.ssh/custom_key
+
+chmod 400 ~/.ssh/custom_key
+
+IFS=',' ;for element in ${CUSTOM_SSH_KEY_HOSTS};
+do
+echo -e "Host $element\n"\
+        "  IdentityFile ~/.ssh/custom_key\n"\
+        "  IdentitiesOnly yes\n"\
+        "  UserKnownHostsFile=/dev/null\n"\
+        "  StrictHostKeyChecking no"\
+        >> ~/.ssh/config
+done
+
+echo "-----> Successfully added custom SSH key"
+
 pip install git+https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/MycroftKang/highlighter-core.git@fec30ae4b42480a0cc99438db062c8b4871c8a9f
